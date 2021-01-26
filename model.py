e6ba23061e3e54b5a3fd368713f27ed84deba73c
Aishwarya-15 <aishwaryalakshmi1200@gmail.com>
2021-01-26 02:16:43 -0800

    First commit

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.svm import SVC
 import pickle
 
-df = pd.read_csv('C:\\Users\\ELCOT\\Downloads\\datasets_19_420_Iris.csv')
+df = pd.read_csv('C:\Users\ELCOT\Downloads\datasets_19_420_Iris.csv')
 
 X = df.drop(['Id','Species'],axis=1).astype(float)
 y = df['Species']
